cad46acc7c2c01c1fc1ec7561d4fd0d4af0601c2
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-08-19 18:40:43 +0200

    feat: use Astro's incremental builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://favorite-contributors.netlify.app"
+  site: "https://favorite-contributors.netlify.app",
+  experimental: {
+    incrementalBuild: true,
+  }
 });
