2c842df1df4e62958679f5f52cb645c2aa39832f
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-08-29 16:46:13 +0200

    fix: limit matrix to 1

--- scripts/utils/consts.ts
+++ scripts/utils/consts.ts
@@ -1,5 +1,5 @@
 export const CONCURRENCY_LIMIT = 5;
-export const MAX_MATRIX_SIZE = 3;
+export const MAX_MATRIX_SIZE = 1;
 export const MIN_STARS_THRESHOLD = 69;
 export const MAX_ORGS_TO_CRAWL_PER_RUN = 5;
 export const MIN_CONTRIBUTORS_THRESHOLD = 42;
